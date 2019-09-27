6f791749549edba821719fece1630319efed97d2
Igor7086 <49655092+Igor7086@users.noreply.github.com>
2019-09-27 22:56:08 +0300

    есть такой метод format ) @elpiankova

--- Task1_4.py
+++ Task1_4.py
@@ -1,8 +1,2 @@
 a = "Homework"
-all_numbers = 100
-
-half_space_numbers = " "*((all_numbers - len(a))//2)
-a_with_spaces = half_space_numbers + a + half_space_numbers 
-
-print(a_with_spaces)
-
+print("'", '{:^100}'.format(a), "'")
